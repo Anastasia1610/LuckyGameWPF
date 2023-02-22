6d6656392315ca882ad99874bfbfd4fa4273b826
пк <@DESKTOP-70U3VC3>
2023-02-22 13:12:59 +0200

    Добавьте файлы проекта.

---
+++ LuckyGame/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LuckyGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            HidenNum = random.Next(100);
+        }
+
+        private int HidenNum;
+
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            if (Info.Text == "Win")
+            {
+                Info.Text = "Угадай число!";
+                Field.Text = "";
+                Random random = new Random();
+                HidenNum = random.Next(100);
+                return;
+            }
+
+            if (int.TryParse((Field.Text).ToString(), out int Number))
+            {
+                if (Number == HidenNum)
+                {
+                    Info.Text = "Win";
+                    Field.Text = "";
+                }
+                else if (Number < HidenNum)
+                {
+                    Info.Text = "Try more";
+                    Field.Text = "";
+                }
+                else
+                {
+                    Info.Text = "Try less";
+                    Field.Text = "";
+                }
+            }
+            else
+                Field.Text = "";
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Text = "Угадай число!";
+            Field.Text = "";
+            Random random = new Random();
+            HidenNum = random.Next(100);
+        }
+    }
+}
